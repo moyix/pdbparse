5af49cf4215a3617bc6312f2739b78716b721404
mooyix <mooyix@59fe9168-b83d-0410-a727-33fbc2eb2a91>
2007-11-29 22:31:36 +0000

    Fixed small bug--gsym_file is a short, not a dword.

--- print_gvars.py
+++ print_gvars.py
@@ -8,10 +8,19 @@ pdb = pdbparse.parse(sys.argv[1])
 pe = PE(sys.argv[2])
 gsyms = pdb.streams[pdb.streams[3].gsym_file]
 
-for k in sorted(gsyms.vars.keys()):
-    off = gsyms.vars[k].offset
+#for k in sorted(gsyms.vars.keys()):
+#    off = gsyms.vars[k].offset
+#    try:
+#        virt_base = pe.sections[gsyms.vars[k].segment-1].VirtualAddress
+#        print "%-60s: %#08x" % (k, off+virt_base)
+#    except IndexError:
+#        print "Skipping %s, segment %d does not exist" % (k,gsyms.vars[k].segment-1)
+
+for sym in gsyms.globals:
+    off = sym.offset
     try:
-        virt_base = pe.sections[gsyms.vars[k].segment-1].VirtualAddress
-        print "%-60s: %#08x" % (k, off+virt_base)
+        virt_base = pe.sections[sym.segment-1].VirtualAddress
+        print "%-60s: %#08x" % (sym.name, off+virt_base)
     except IndexError:
-        print "Skipping %s, segment %d does not exist" % (k,gsyms.vars[k].segment-1)
+        print "Skipping %s, segment %d does not exist" % (sym.name,sym.segment-1)
+
