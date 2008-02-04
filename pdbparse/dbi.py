cf1b2588827d39733bfb312c88312bc47c3a4776
mooyix <mooyix@59fe9168-b83d-0410-a727-33fbc2eb2a91>
2008-02-04 19:57:14 +0000

    Support more of the DBI stream.

--- pdbparse/dbi.py
+++ pdbparse/dbi.py
@@ -2,14 +2,19 @@
 
 from construct import *
 
+_ALIGN = 4
+
+def get_parsed_size(tp,con):
+    return len(tp.build(con))
+
 def SymbolRange(name): 
     return Struct(name,
-        ULInt16("segment"),
+        SLInt16("segment"),
         Padding(2),
         ULInt32("offset"),
-        ULInt32("size"),
+        SLInt32("size"),
         ULInt32("characteristics"),
-        ULInt16("index"),
+        SLInt16("index"),
         Padding(2),
         ULInt32("timestamp"),
         ULInt32("unknown"),
@@ -21,9 +26,9 @@ DBIHeader = Struct("DBIHeader",
     ULInt32("unknown"),
     ULInt32("hash1_file"),
     ULInt32("hash2_file"),
-    ULInt16("gsym_file"),
+    ULInt16("gsym_file"),           # stream containing global symbols
     ULInt16("unknown2"),
-    ULInt32("module_size"),
+    ULInt32("module_size"),         # total size of DBIExHeaders
     ULInt32("offset_size"),
     ULInt32("hash_size"),
     ULInt32("srcmodule_size"),
@@ -42,17 +47,30 @@ DBIExHeader = Struct("DBIExHeader",
     ULInt32("nSrcFiles"),
     ULInt32("attribute"),
     Array(2, ULInt32("reserved")),
-    CString("filename1"),
-    Aligned(CString("filename2")),
+    Array(2,
+        CString("filenames"),
+    ),
 )
 
 DBI = Debugger(Struct("DBI",
     DBIHeader,
-    #GreedyRange(DBIExHeader),
 ))
 
 def parse_stream(stream):
-    return DBI.parse_stream(stream)
+    pos = 0
+    dbihdr = DBIHeader.parse_stream(stream)
+    pos += get_parsed_size(DBIHeader, dbihdr)
+    stream.seek(pos)
+    dbiexhdr_data = stream.read(dbihdr.module_size)
+
+    dbiexhdrs = []
+    while dbiexhdr_data:
+        dbiexhdrs.append(DBIExHeader.parse(dbiexhdr_data))
+        sz = get_parsed_size(DBIExHeader,dbiexhdrs[-1])
+        if sz % _ALIGN != 0: sz = sz + (_ALIGN - (sz % _ALIGN))
+        dbiexhdr_data = dbiexhdr_data[sz:]
+    print stream.tell()
+    return Container(DBIHeader = dbihdr, DBIExHeaders = ListContainer(dbiexhdrs))
 
 def parse(data):
-    return DBI.parse(data)
+    return parse_stream(StringIO(data))
