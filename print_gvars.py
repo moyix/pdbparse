ca73a0951746bbbdef968dec4d1d25f591574feb
mooyix <mooyix@59fe9168-b83d-0410-a727-33fbc2eb2a91>
2008-12-01 03:22:14 +0000

    Some minor changes.

--- print_gvars.py
+++ print_gvars.py
@@ -2,31 +2,24 @@
 
 import sys
 import pdbparse
-from pefile import PE
+from pdbparse.pe import Sections
+from pdbparse.omap import remap,OMAP_FROM_SRC
 
 def cstring(str):
-    for i in range(len(str)):
-        if str[i] == '\0': return str[:i]
-    return str
+    return str.split('\0')[0]
 
 pdb = pdbparse.parse(sys.argv[1])
-pe = PE(sys.argv[2])
+print len(pdb.streams)
+sects = Sections.parse(pdb.streams[10].data)
 gsyms = pdb.streams[pdb.streams[3].gsym_file]
-
-#for k in sorted(gsyms.vars.keys()):
-#    off = gsyms.vars[k].offset
-#    try:
-#        virt_base = pe.sections[gsyms.vars[k].segment-1].VirtualAddress
-#        print "%-60s: %#08x" % (k, off+virt_base)
-#    except IndexError:
-#        print "Skipping %s, segment %d does not exist" % (k,gsyms.vars[k].segment-1)
+omap = OMAP_FROM_SRC.parse(pdb.streams[12].data)
 
 for sym in gsyms.globals:
     off = sym.offset
     try:
-        virt_base = pe.sections[sym.segment-1].VirtualAddress
-        nm = cstring(pe.sections[sym.segment-1].Name)
-        print "%-40s: (%s+%x) = (%x+%x) = %x" % (sym.name,nm,off,virt_base,off,off+virt_base)
+        virt_base = sects[sym.segment-1].VirtualAddress
+        nm = cstring(sects[sym.segment-1].Name)
+        #print "%-40s: (%s+%x) = (%x+%x) = %x (pre-OMAP)" % (sym.name,nm,off,virt_base,off,off+virt_base)
+        print "%s,%x,%d,%s" % (sym.name,remap(off+virt_base,omap),sym.symtype,nm)
     except IndexError:
-        print "Skipping %s, segment %d does not exist" % (sym.name,sym.segment-1)
-
+        print >> sys.stderr, "Skipping %s, segment %d does not exist" % (sym.name,sym.segment-1)
