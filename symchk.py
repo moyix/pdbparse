2542442f5b51345daef3a641fabd214a5ba36494
mooyix <mooyix@59fe9168-b83d-0410-a727-33fbc2eb2a91>
2009-07-20 15:20:51 +0000

    Incorporate patch from Jesse Kornblum to add error checking and documentation

--- symchk.py
+++ symchk.py
@@ -1,16 +1,57 @@
 #!/usr/bin/env python
 
+''' 
+Download debugging symbols from the Microsoft Symbol Server. 
+Can use as an input an executable file OR a GUID+Age and filename.
+Examples:
+
+$ python symcheck.py -e ntoskrnl.exe
+
+$ python symchk.py -g 32962337f0f646388b39535cd8dd70e82 -s ntoskrnl.pdb
+The GUID+Age here corresponds to the kernel version of the xp-laptop-2005-* images
+The Age value is 0x2.
+
+
+Module Dependencies:
+This script requires the following modules:
+pefile - http://code.google.com/p/pefile/
+construct - http://construct.wikispaces.com/
+To decompress downloaded files you should also have cabextract on your system.
+http://www.cabextract.org.uk/
+
+License: 
+GPL version 3
+http://www.gnu.org/licenses/gpl.html
+
+Miscellaneous References:
+You can see an explanation of the URL format at:
+http://jimmers.info/pdb.html
+'''
+
 import urllib2
 import sys,os
 import os.path
 from pefile import PE
 from shutil import copyfileobj
+from urllib import FancyURLopener
 from pdbparse.dbgold import CV_RSDS_HEADER, CV_NB10_HEADER, DebugDirectoryType
 
-UA = "Microsoft-Symbol-Server/6.6.0007.5"
+USER_AGENT = "Microsoft-Symbol-Server/6.6.0007.5"
+
+class PDBOpener(FancyURLopener):
+    version = USER_AGENT
+    def http_error_default(self, url, fp, errcode, errmsg, headers):
+        if errcode == 404:
+            raise urllib2.HTTPError(url, errcode, errmsg, headers, fp)
+        else:
+            FancyURLopener.http_error_default(url, fp, errcode, errmsg, headers)
 
 def get_pe_debug_data(filename):
-    pe = PE(filename)
+    try:
+        pe = PE(filename)
+    except IOError, e:
+        print e
+        sys.exit(-1)
     dbgstruct = pe.DIRECTORY_ENTRY_DEBUG[0].struct
     addr = dbgstruct.PointerToRawData
     sz = dbgstruct.SizeOfData
@@ -20,27 +61,50 @@ def get_pe_debug_data(filename):
     f.close()
     return dbgdata, DebugDirectoryType._decode(dbgstruct.Type,{})
 
+lastprog = None
+def progress(blocks,blocksz,totalsz):
+    global lastprog
+    if lastprog is None:
+        print "Connected. Downloading data..."
+    percent = int((100*(blocks*blocksz)/float(totalsz)))
+    if lastprog != percent and percent % 5 == 0: print "%d%%" % percent,
+    lastprog = percent
+    sys.stdout.flush()
+
 def download_file(guid,fname,path=""):
+    ''' 
+    Download the symbols specified by guid and filename. Note that 'guid'
+    must be the GUID from the executable with the dashes removed *AND* the
+    Age field appended. The resulting file will be saved to the path argument,
+    which default to the current directory.
+    '''
+    
+    # A normal GUID is 32 bytes. With the age field appended
+    # the GUID argument should therefore be longer to be valid.
+    # Exception: old-style PEs without a debug section use 
+    # TimeDateStamp+SizeOfImage
+    if len(guid) < 33:
+        print "Warning: GUID is too short to be valid. Did you append the Age field?"
+        print "(you can safely ignore this warning if using TimeDateStamp+SizeOfImage)"
+
     url = "http://msdl.microsoft.com/download/symbols/%s/%s/" % (fname,guid)
     opener = urllib2.build_opener()
-    tries = [ fname, fname[:-1] + '_', 'file.ptr' ]
+    
+    # Whatever extension the user has supplied it must be replaced with .pd_
+    tries = [ fname[:-1] + '_', fname ]
 
     for t in tries:
-        request = urllib2.Request(url+t)
         print "Trying %s" % (url+t)
-        request.add_header('User-agent', UA)
         try:
-            op = opener.open(request)
-            f = open(path+t, 'w')
-            copyfileobj(op,f)
-            #f.write(op.read())
-            f.close()
-            op.close()
+            PDBOpener().retrieve(url+t, path+t, reporthook=progress)
+            print
             print "Saved symbols to %s" % (path+t)
             return path+t
-        except urllib2.HTTPError:
+        except urllib2.HTTPError, e:
+            print "HTTP error %u" % (e.code)
             pass
-    return False
+    return None
+
 
 def get_dbg_fname(dbgdata):
     from pdbparse.dbgold import IMAGE_DEBUG_MISC
@@ -62,7 +126,11 @@ def get_nb10(dbgdata):
     return guidstr,dbg.Filename
 
 def get_pe_guid(filename):
-    pe = PE(filename)
+    try:
+        pe = PE(filename)
+    except IOError, e:
+        print e
+        sys.exit(-1)
     guidstr = "%x%x" % (pe.FILE_HEADER.TimeDateStamp,
                         pe.OPTIONAL_HEADER.SizeOfImage)
     return guidstr
@@ -113,18 +181,19 @@ def get_pe_from_pe(filename):
     if saved_file.endswith("_"):
         os.system("cabextract %s" % saved_file)
 
-if __name__ == "__main__":
+
+def main():
     from optparse import OptionParser
 
     parser = OptionParser()
     parser.add_option('-e', '--executable', dest='exe',
-            help='download symbols for an executable')
+                      help='download symbols for an executable')
     parser.add_option('-p', '--pefile', dest='pe',
-            help='download clean copy of an executable')
+                      help='download clean copy of an executable')
     parser.add_option('-g', '--guid', dest='guid',
-            help='use GUID to download symbols [Note: requires -s]')
+                      help='use GUID to download symbols [Note: requires -s]')
     parser.add_option('-s', '--symbols', dest='symfile', metavar='FILENAME',
-            help='use FILENAME to download symbols [Note: requires -g]')
+                      help='use FILENAME to download symbols [Note: requires -g]')
 
     opts,args = parser.parse_args()
 
@@ -134,10 +203,16 @@ if __name__ == "__main__":
         get_pe_from_pe(opts.pe)
     if opts.guid and opts.symfile:
         saved_file = download_file(opts.guid, opts.symfile)
-        if saved_file.endswith("_"):
-            os.system("cabextract %s" % saved_file)
+        if saved_file is not None:
+            if saved_file.endswith("_"):
+                os.system("cabextract %s" % saved_file)
     
     if not (opts.exe or opts.guid or opts.symfile or opts.pe) and args:
         for a in args: handle_pe(a)
     elif not (opts.exe or opts.guid or opts.symfile or opts.pe) and not args:
         parser.error("Must supply a PE file or specify by GUID")
+
+
+if __name__ == "__main__":
+    main()
+
