d096a8aa2a1b5db65632b0adbc673d108880e9b8
mooyix@gmail.com <mooyix@gmail.com@59fe9168-b83d-0410-a727-33fbc2eb2a91>
2013-02-27 21:56:29 +0000

    Adding C undecorate library to undecorate mangled MSVC++ names

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, Extension
 
 setup(name='pdbparse',
       version='1.1',
@@ -9,10 +9,24 @@ setup(name='pdbparse',
       author_email='brendandg@gatech.edu',
       url='http://pdbparse.googlecode.com/',
       packages=['pdbparse'],
-      requires=['construct'],
+      requires=['construct', 'pefile'],
       classifiers=[
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
       ],
+      ext_modules=[
+        Extension('pdbparse._undname', ['src/undname.c'], export_symbols=['undname'])
+      ],
+      scripts=[
+        'examples/get_syscall_table.py',
+        'examples/lookup.py',
+        'examples/pdbdump.py',
+        'examples/print_ctypes.py',
+        'examples/print_gvars.py',
+        'examples/print_tpi.py',
+        'examples/symchk.py',
+        'examples/tpi_closure.py',
+        'examples/tpi_vtypes.py',
+      ]
      )
 
