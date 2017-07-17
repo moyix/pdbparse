aa2c1f4b57d01b57c04d12f5c54fef6b2014f789
lucasg <lucas.georges@outlook.com>
2017-07-17 17:37:13 +0200

    Define print_gvars main entry point

--- examples/pdb_print_gvars.py
+++ examples/pdb_print_gvars.py
@@ -15,33 +15,40 @@ class DummyOmap(object):
 def cstring(str):
     return str.split(b'\0')[0]
 
-parser = OptionParser()
-(opts, args) = parser.parse_args()
+    
 
-if len(args) != 2:
-    parser.error("Need filename and base address")
-
-pdb = pdbparse.parse(args[0])
-imgbase = int(args[1], 0)
-try:
-    sects = pdb.STREAM_SECT_HDR_ORIG.sections
-    omap = pdb.STREAM_OMAP_FROM_SRC
-except AttributeError as e:
-    # In this case there is no OMAP, so we use the given section
-    # headers and use the identity function for omap.remap
-    sects = pdb.STREAM_SECT_HDR.sections
-    omap = DummyOmap()
-
-gsyms = pdb.STREAM_GSYM
-
-
-for sym in gsyms.globals:
+def main(filename, base_address):
+    pdb = pdbparse.parse(filename)
+    imgbase = int(base_address, 0)
     try:
-        off = sym.offset
-        virt_base = sects[sym.segment-1].VirtualAddress
-        nm = cstring(sects[sym.segment-1].Name)
-        print ("%s,%#x,%d,%s" % (sym.name,imgbase+omap.remap(off+virt_base),sym.symtype,nm))
-    except IndexError  as e:
-        print ("Skipping %s, segment %d does not exist" % (sym.name,sym.segment-1), file=sys.stderr)
+        sects = pdb.STREAM_SECT_HDR_ORIG.sections
+        omap = pdb.STREAM_OMAP_FROM_SRC
     except AttributeError as e:
-        pass
+        # In this case there is no OMAP, so we use the given section
+        # headers and use the identity function for omap.remap
+        sects = pdb.STREAM_SECT_HDR.sections
+        omap = DummyOmap()
+
+    gsyms = pdb.STREAM_GSYM
+
+
+    for sym in gsyms.globals:
+        try:
+            off = sym.offset
+            virt_base = sects[sym.segment-1].VirtualAddress
+            nm = cstring(sects[sym.segment-1].Name)
+            print ("%s,%#x,%d,%s" % (sym.name,imgbase+omap.remap(off+virt_base),sym.symtype,nm))
+        except IndexError  as e:
+            print ("Skipping %s, segment %d does not exist" % (sym.name,sym.segment-1), file=sys.stderr)
+        except AttributeError as e:
+            pass
+
+if __name__ == '__main__':
+
+    parser = OptionParser()
+    (opts, args) = parser.parse_args()
+
+    if len(args) != 2:
+        parser.error("Need filename and base address")
+
+    main(args[0], args[1])
