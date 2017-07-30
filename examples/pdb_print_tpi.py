11d8a1640279e66f64dbda30761fdd18bf989958
Brendan Dolan-Gavitt <brendandg@nyu.edu>
2017-07-30 13:54:42 -0400

    Fix up \xff -> -1 conversion in Enumerations

--- examples/pdb_print_tpi.py
+++ examples/pdb_print_tpi.py
@@ -123,7 +123,7 @@ def enum_pretty_str(enum):
     enumerated = []
     utypename = get_tpname(enum.utype)
     for e in enum.fieldlist.substructs:
-        e_val = -1 if e.enum_value == '\xff' else e.enum_value
+        e_val = -1 if e.enum_value == b'\xff' else e.enum_value
         enumerated.append("%s = %d" % (e.name, e_val))
     return (enum.name + (" (%s)\n    " % utypename) + 
             "\n    ".join(enumerated))
