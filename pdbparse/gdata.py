b34608757b1b45c1538289bca2cf4654fff9618f
Mike Auty <mike.auty@gmail.com>
2019-04-21 23:39:28 +0100

    Do some horrible tricks to merge the parsed symbols.
    As of construct-2.9 (unmaintained) the only person working on it won't
    touch "embedding" with a barge pole.  Embedded essentially changes the
    hierarchy, so instead we do that manually with the parsed output after
    parsing.

--- pdbparse/gdata.py
+++ pdbparse/gdata.py
@@ -2,7 +2,7 @@
 
 from construct import *
 
-gsym = "global" / Struct(
+gsym = Struct(
     "leaf_type" / Int16ul, "data" / Switch(
         lambda ctx: ctx.leaf_type, {
             0x110E:
@@ -17,22 +17,38 @@ gsym = "global" / Struct(
                 "symtype" / Int32ul,
                 "offset" / Int32ul,
                 "segment" / Int16ul,
-                "name" / PascalString(lengthfield = "len" / Int8ul, encoding = "utf8"),
+                "name" / PascalString(lengthfield = "length" / Int8ul, encoding = "utf8"),
             ),
         }))
 
-GlobalsData = GreedyRange(
-    RestreamData(
-        "globals" / PascalString(lengthfield = "len" / Int16ul, encoding = "utf8"),
-        gsym,
+GlobalsData = "globals" / GreedyRange(
+    Struct(
+        "length" / Int16ul,
+        "symbol" / RestreamData(Bytes(lambda ctx: ctx.length), gsym),
     ))
 
 
 def parse(data):
     con = GlobalsData.parse(data)
-    return con
+    return merge_structures(con)
 
 
 def parse_stream(stream):
     con = GlobalsData.parse_stream(stream)
-    return con
+    return merge_structures(con)
+
+
+def merge_structures(con):
+    new_cons = []
+    for sym in con:
+        sym_dict = {'length': sym.length, 'leaf_type': sym.symbol.leaf_type}
+        if sym.symbol.data:
+            sym_dict.update({
+                'symtype': sym.symbol.data.symtype,
+                'offset': sym.symbol.data.offset,
+                'segment': sym.symbol.data.segment,
+                'name': sym.symbol.data.name
+            })
+        new_cons.append(Container(sym_dict))
+    result = ListContainer(new_cons)
+    return result
